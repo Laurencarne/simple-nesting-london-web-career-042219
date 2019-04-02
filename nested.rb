c49d02c09faf8a137ddc7e57d8477e64d64481cd
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-02 14:12:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,8 +62,6 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -79,8 +77,7 @@ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LI
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
 def changing_alan
